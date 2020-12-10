48c3b9c0935140d444144823be4d8bcec9071f19
Songly <74387463+Songlyy@users.noreply.github.com>
2020-12-10 16:49:06 -0300

    *

---
+++ sw.js
@@ -0,0 +1,6 @@
+'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js');
+
+const cacheable = ['/index.html', '/sw.js', '/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open('app').then(ch => ch.addAll(cacheable)).then(self.skipWaiting())));
+
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)))
