de4bf4646efcdc56fdc3d6b49e983969e6f309b2
Joaa <Joaquinuriel94@gmail.com>
2020-12-11 11:47:02 -0300

    *

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 'serviceWorker' in navigator && navigator.serviceWorker.register('/sw.js');
 
 self.addEventListener('install', e => e.waitUntil(caches.open('app').then(ch => ch.addAll(['/sw.js', '/manifest.json'])).then(self.skipWaiting())));
-
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)
-    .then(res => caches.open('fallback').then(cache => cache.put(e.request, res.clone()) && res))
-    .catch(() => caches.match(e.request))
-))
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)))
+// self.addEventListener('fetch', e => e.respondWith(fetch(e.request)
+//     .then(res => caches.open('fallback').then(cache => cache.put(e.request, res.clone()) && res))
+//     .catch(() => caches.match(e.request))
+// ))
