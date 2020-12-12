5582f05887cda06c35ce63f1a9bd8e8e4dc65e94
Joaa <Joaquinuriel94@gmail.com>
2020-12-12 19:33:12 -0300

    *

--- app.js
+++ app.js
@@ -8,26 +8,4 @@ const aa = header.querySelectorAll('a');
 window.onscroll = () => window.scrollY > 0 ?
     (header.style.backgroundColor = "#fffe", header.style.color = "royalblue") :
     (header.style.backgroundColor = "#0fa", header.style.color = "white");
-const qr = header.querySelector('nav svg');
-qr.addEventListener('click', () => qr.classList.toggle('qr'));
-let socket = new WebSocket("wss://joaquinuriel.github.io");
-socket.onopen = (e) => {
-    alert("[open] Connection established");
-    alert("Sending to server");
-    socket.send("My name is John");
-};
-socket.onmessage = (event) => {
-    alert(`[message] Data received from server: ${event.data}`);
-};
-socket.onclose = (event) => {
-    if (event.wasClean) {
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    }
-    else {
-        alert('[close] Connection died');
-    }
-};
-socket.onerror = (error) => {
-    alert(`[error] ${error.message}`);
-};
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,5 +1,3 @@
-// setTimeout((ss = document.querySelectorAll('s')) => ss.forEach(async s => s.outerHTML = localStorage.getItem(s.innerHTML) || 
-// await fetch(`/assets/ionicons/${s.innerHTML}.svg`).then(res => res.text().then(txt => (localStorage.setItem(s.innerHTML, txt), txt)))));
 const ss = document.querySelectorAll('s')
 ss.forEach(async s => s.outerHTML = localStorage.getItem(s.innerHTML) || await fetch(`/assets/ionicons/${s.innerHTML}.svg`)
     .then(res => res.text().then(txt => (localStorage.setItem(s.innerHTML, txt), txt))))
@@ -11,30 +9,27 @@ const aa = header.querySelectorAll('a')
 window.onscroll = () => window.scrollY > 0? 
     (header.style.backgroundColor = "#fffe", header.style.color = "royalblue") :
     (header.style.backgroundColor = "#0fa", header.style.color = "white")
-const qr = header.querySelector('nav svg')
-qr.addEventListener('click', () => qr.classList.toggle('qr'))
+
+// const qr = header.querySelector('nav svg')
+// qr.addEventListener('click', () => qr.classList.toggle('qr'))
 // header.addEventListener('click', e => qr.contains(e.target) || qr.classList.toggle('qr'))
 
-let socket = new WebSocket("wss://joaquinuriel.github.io");
-
-socket.onopen = (e) => {
-    alert("[open] Connection established")
-    alert("Sending to server")
-    socket.send("My name is John")
-};
-
-socket.onmessage = (event) => {
-    alert(`[message] Data received from server: ${event.data}`)
-};
-
-socket.onclose = (event) => {
-    if (event.wasClean) {
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
-    } else {
-        alert('[close] Connection died')
-    }
-};
-
-socket.onerror = (error) => {
-    alert(`[error] ${error.message}`)
-};
+// let socket = new WebSocket("wss://joaquinuriel.github.io");
+// socket.onopen = (e) => {
+//     alert("[open] Connection established")
+//     alert("Sending to server")
+//     socket.send("My name is John")
+// };
+// socket.onmessage = (event) => {
+//     alert(`[message] Data received from server: ${event.data}`)
+// };
+// socket.onclose = (event) => {
+//     if (event.wasClean) {
+//         alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
+//     } else {
+//         alert('[close] Connection died')
+//     }
+// };
+// socket.onerror = (error) => {
+//     alert(`[error] ${error.message}`)
+// };
