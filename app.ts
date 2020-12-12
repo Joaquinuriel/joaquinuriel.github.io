6da4792e21ac886c8911aa7dc130a5d968101146
Joaa <Joaquinuriel94@gmail.com>
2020-12-12 10:37:16 -0300

    *

---
+++ app.ts
@@ -0,0 +1,13 @@
+// setTimeout((ss = document.querySelectorAll('s')) => ss.forEach(async s => s.outerHTML = localStorage.getItem(s.innerHTML) || 
+// await fetch(`/assets/ionicons/${s.innerHTML}.svg`).then(res => res.text().then(txt => (localStorage.setItem(s.innerHTML, txt), txt)))));
+const ss = document.querySelectorAll('s')
+ss.forEach(async s => s.outerHTML = localStorage.getItem(s.innerHTML) || await fetch(`/assets/ionicons/${s.innerHTML}.svg`)
+    .then(res => res.text().then(txt => (localStorage.setItem(s.innerHTML, txt), txt))))
+    
+const menu = document.querySelector('header svg')
+menu.addEventListener('click', () => header.classList.toggle('open'))
+const header = document.querySelector('header')
+const aa = header.querySelectorAll('a')
+window.onscroll = () => window.scrollY > 0? 
+    (header.style.backgroundColor = "#fffe", header.style.color = "royalblue") :
+    (header.style.backgroundColor = "#0fa", header.style.color = "white")
