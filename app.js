d2fdabe147ef969cda0ab5c706fcb80cba7b610e
Joaa <Joaquinuriel94@gmail.com>
2020-12-11 22:13:19 -0300

    *

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 setTimeout((ss = document.querySelectorAll('s')) => ss.forEach(async s => s.outerHTML = localStorage.getItem(s.innerHTML) || 
-await fetch(`/assets/ionicons/${s.innerHTML}.svg`).then(res => res.text().then(txt => localStorage.setItem(s.innerHTML, txt) && txt))))
+await fetch(`/assets/ionicons/${s.innerHTML}.svg`).then(res => res.text().then(txt => localStorage.setItem(s.innerHTML, txt) || txt))))
 
 setTimeout((menu = document.querySelector('header svg')) => menu.onclick = () => header.classList.toggle('open'))
 const header = document.querySelector('header')
