097f56854f11e5d74075cce7b68b7a31c3c7d970
Joaquin Martinez <joaquinuriel94@gmail.com>
2021-02-22 14:06:30 -0300

    *

--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
 navigator.serviceWorker.register('/sw.js');
 
 // ICONS
-let icons = document.querySelectorAll('s')
-icons.forEach(async icon => icon.outerHTML = await getIcon(icon.innerHTML))
-
 const getIcon = async (icon: string) => localStorage.getItem(icon) || await fetch(`/assets/ionicons/${icon}.svg`)
     .then(res => res.text().then(txt => (localStorage.setItem(icon, txt), txt)))
 
+let icons = document.querySelectorAll('s')
+icons.forEach(async icon => icon.outerHTML = await getIcon(icon.innerHTML))
+
 let header = document.querySelector("header")
-let menu = header.querySelector("svg")
+let headerBtn = header.querySelector("button")
+headerBtn.addEventListener("click", () => menu.classList.toggle("--hidden"))
 
+let menu = document.getElementById("menu")
+let menuBtn = document.getElementById("close-menu")
+menuBtn.addEventListener("click", () => menu.classList.toggle("--hidden"))
 
-// menu.addEventListener('click', () => menu.ac)
-
+let clearCache = document.getElementById("clear-cache")
+clearCache.addEventListener("click", () => localStorage.clear())
