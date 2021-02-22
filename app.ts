79ab421088ea469930e6a7fd962ebef54d2256e8
Joaquin Martinez <joaquinuriel94@gmail.com>
2021-02-22 19:52:58 -0300

    *

--- app.ts
+++ app.ts
@@ -9,11 +9,11 @@ icons.forEach(async icon => icon.outerHTML = await getIcon(icon.innerHTML))
 
 let header = document.querySelector("header")
 let headerBtn = header.querySelector("button")
-headerBtn.addEventListener("click", () => menu.classList.toggle("--hidden"))
+headerBtn.addEventListener("click", () => menu.classList.toggle("hidden"))
 
 let menu = document.getElementById("menu")
 let menuBtn = document.getElementById("close-menu")
-menuBtn.addEventListener("click", () => menu.classList.toggle("--hidden"))
+menuBtn.addEventListener("click", () => menu.classList.toggle("hidden"))
 
 let clearCache = document.getElementById("clear-cache")
 clearCache.addEventListener("click", () => localStorage.clear())
