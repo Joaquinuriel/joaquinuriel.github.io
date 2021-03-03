2970219705c13ad40c47e6a122da77f81dcf347a
Joaquin Martinez <joaquinuriel94@gmail.com>
2021-03-03 16:27:34 -0300

    *

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # joaquinuriel.github.io
 ## Personal web app
-### Not all those who wonder are lost
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,23 @@
-navigator.serviceWorker.register('/sw.js');
 const getIcon = async (icon) => localStorage.getItem(icon) || await fetch(`/assets/ionicons/${icon}.svg`)
     .then(res => res.text().then(txt => (localStorage.setItem(icon, txt), txt)));
+
 let icons = document.querySelectorAll('s');
 icons.forEach(async (icon) => icon.outerHTML = await getIcon(icon.innerHTML));
+
 let header = document.querySelector("header");
 let menu = document.getElementById("menu");
 let headerBtn = header.querySelector("button");
 let links = menu.querySelectorAll("a");
+
 const toggleMenu = () => {
     document.body.classList.toggle("locked");
     menu.classList.toggle("menu");
     menu.classList.toggle("menu--hidden");
 };
+
 headerBtn.addEventListener("click", () => toggleMenu());
 links.forEach(link => link.addEventListener("click", () => toggleMenu()));
+
 let body = document.body;
 let toast = document.getElementById("toast");
 let para = toast.querySelector("p");
@@ -21,8 +25,8 @@ const say = (message) => {
     para.innerHTML = message;
     toast.style.top = "60px";
 };
+
 let flexBtn = document.querySelector(".flex button");
+
 flexBtn.addEventListener("click", () => say("hello there"));
-toast.addEventListener("click", () => {
-    toast.style.top = null;
-});
+toast.addEventListener("click", () => toast.style.top = null);
