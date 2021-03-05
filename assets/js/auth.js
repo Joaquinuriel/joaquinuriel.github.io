52eee0f48e5820f3891e4d59b9073c7af26e2ac0
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-03-05 19:38:22 -0300

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,27 +1,7 @@
 const auth = () => {
 	let auth = firebase.auth();
 	auth.onAuthStateChanged((user) => {
-		console.log("user: " + user.displayName);
-
-		let signIn = document.getElementById("sign-in");
-		signIn &&
-			signIn.addEventListener("click", () => {
-				let email = prompt("email");
-				let password = prompt("password");
-				auth.signInWithEmailAndPassword(email, password)
-					.then((user) => console.log(user))
-					.catch((error) => console.log(error.message));
-			});
-
-		let signUp = document.getElementById("sign-up");
-		signUp &&
-			signUp.addEventListener("click", () => {
-				let email = prompt("email");
-				let password = prompt("password");
-				auth.createUserWithEmailAndPassword(email, password)
-					.then((user) => console.log(user))
-					.catch((error) => console.log(error.message));
-			});
+		console.log(user);
 
 		let googleBtn = document.getElementById("google-btn");
 		googleBtn &&
