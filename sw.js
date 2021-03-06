19ef5a7688f2ba82598845e341f7c53717709826
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-03-06 00:32:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-// self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request)));
-
-// self.addEventListener("fetch", (event) => {
-//     event.respondWith(
-//         caches.open("stale").then((cache) => cache.match(event.request).then((response) => {
-//             var fetchPromise = fetch(event.request).then((networkResponse) => {
-//                 cache.put(event.request, networkResponse.clone());
-//                 return networkResponse;
-//             });
-//             return response || fetchPromise;
-//         }))
-//     );
+// self.addEventListener('fetch', event => {
+//     if(event.request.method === 'GET') {
+//         if (event.request.url.startsWith(self.location.origin)) {
+//             if (!event.request.url.endsWith("/")) {
+//                 event.respondWith(fetch(event.request));
+//            }
+//         }
+//     }
 // });
+
+// self.addEventListener("fetch", (e) => {
+// 	e.request.url.endsWith("/") || e.respondWith(fetch(e.request));
+// });
+
+self.addEventListener("install", (event) =>
+	event.waitUntil(
+		caches
+			.open("app")
+            .then((cache) => cache.add("/")
+                .then(self.skipWaiting()))
+	)
+);
+
+self.addEventListener("fetch", (event) => {
+	event.request.url.endsWith("/") ||
+        event.respondWith(
+			caches.open("stale").then((cache) =>
+				cache.match(event.request).then((response) => {
+					var fetchPromise = fetch(event.request).then(
+						(networkResponse) => {
+							cache.put(event.request, networkResponse.clone());
+							return networkResponse;
+						}
+					);
+					return response || fetchPromise;
+				})
+			)
+		);
+});
