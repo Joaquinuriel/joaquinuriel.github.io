f34e69bb765389675390a3cf5ebf4d054ff31a73
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-03-11 15:14:06 -0300

    *

--- @/index.md
+++ @/index.md
@@ -3,3 +3,4 @@
 ###### idk what to write here
 
 [@Joaquinuriel](https://github.com/Joaquinuriel)
+[nav](./nav.html)
--- _/dashboard/app.js
+++ _/dashboard/app.js
@@ -10,16 +10,23 @@ firebase.initializeApp({
 
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) console.log("user");
-    else console.log("no user");
+	else console.log("no user");
 });
 
 let btn = document.querySelector("button");
 btn.onclick = () => firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
 
-let scrollbox = document.querySelector(".scrollbox")
-const message = (message) => {
-    let tweet = document.createElement("p")
-    tweet.classList.add("twet")
-    tweet.textContent = message
-    scrollbox.appendChild(tweet)
-}
+let scrollbox = document.querySelector(".scrollbox");
+let db = firebase.database();
+let root = db.ref();
+const tweet = (message) => {
+    root.child("messages")
+        .push()
+		.then((snap) => {
+			console.log(snap.val());
+		});
+	let text = document.createElement("p");
+	text.classList.add("tweet");
+	text.textContent = message;
+	scrollbox.appendChild(text);
+};
